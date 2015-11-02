6e7fdf2be858f4869df33dccc0435c81d5fd9016
Xylios13 / Gui <gbarreto1992@gmail.com>
2015-11-02 17:10:25 +0000

    move files

--- archigd/main.rkt
+++ archigd/main.rkt
@@ -8,6 +8,17 @@
 (require "rosetta/protobuf1/encoding.rkt")
 (require srfi/26)
 (require "Messages.rkt")
+(require racket/date)
+
+(define (move-addon-file)
+  (let ((internal-path-addon "../x64/Geometry_Test.apx")
+        (internal-path-directory "D:/GRAPHISOFT/ArchiCAD 18/Add-Ons")
+        (internal-path-directory-addon "D:/GRAPHISOFT/ArchiCAD 18/Add-Ons/Geometry_Test.apx"))
+    (when (and (directory-exists? internal-path-directory)
+               (file-exists? internal-path-addon))
+      (rename-file-or-directory internal-path-addon internal-path-directory-addon #t))))
+(move-addon-file)
+
 
 (define current-level (make-parameter #f))
 (define default-level-to-level-height (make-parameter 3))
